5bf9e0101e52eada1579e1954c7e6f2896525a55
pablo <pablo>
2021-02-09 14:03:24 -0400

    Add antd library Add rule for console.

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,12 +1,21 @@
+/* eslint-disable no-console */
+
 import "./App.scss";
 import React from "react";
 import PropTypes from "prop-types";
+import { Button } from "antd";
 
-const Greeting = ({title}) => (
+function Greeting ({title}) {
+  console.log("asas");
+  return (<>
     <div className = "App">
       {title}
     </div>
+  
+    <Button type="primary" onClick = {()=>console.log("asas")}>Button</Button>
+  </>
   );
+}
 
 Greeting.propTypes = {
   title: PropTypes.string
